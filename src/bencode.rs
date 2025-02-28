b98d86cd52f4e650b37bd2425508e2fe8ad5f2f5
Aethne <aethnem@gmail.com>
2025-02-28 15:35:37 -0500

    gitignore

--- src/bencode.rs
+++ src/bencode.rs
@@ -5,7 +5,7 @@ pub enum BencodeObj {
     Num(isize),
     Str(Vec<u8>),
     List(Vec<BencodeObj>),
-    Dict(HashMap<Vec<u8>, Box<BencodeObj>>)
+    Dict(HashMap<Vec<u8>, Box<BencodeObj>>),
 }
 
 #[derive(Debug)]
@@ -18,7 +18,7 @@ enum State {
     RdDic,
 }
 
-pub fn decode(encoded: &[u8]) -> (Result<BencodeObj, ()>, &[u8]/* remaining */) {
+pub fn decode(encoded: &[u8]) -> (Result<BencodeObj, ()>, &[u8] /* remaining */) {
     // 1 pass parser
     let mut state = State::Ready;
     let mut start_pointer: usize = 0;
@@ -31,86 +31,110 @@ pub fn decode(encoded: &[u8]) -> (Result<BencodeObj, ()>, &[u8]/* remaining */) 
 
     for ch in encoded.iter() {
         match state {
-            State::Ready => {
-                match ch {
-                    b'0'..=b'9' => { state = State::RdStrLen; end_pointer += 1; },
-                    b'i'       => { state = State::RdInt; start_pointer += 1; },
-                    b'l'       => { state = State::RdLst; start_pointer += 1; },
-                    b'd'       => { state = State::RdDic; start_pointer += 1; },
-                    _         => { unexpected = true; }
+            State::Ready => match ch {
+                b'0'..=b'9' => {
+                    state = State::RdStrLen;
+                    end_pointer += 1;
+                }
+                b'i' => {
+                    state = State::RdInt;
+                    start_pointer += 1;
+                }
+                b'l' => {
+                    state = State::RdLst;
+                    start_pointer += 1;
+                }
+                b'd' => {
+                    state = State::RdDic;
+                    start_pointer += 1;
+                }
+                _ => {
+                    unexpected = true;
                 }
             },
 
-            State::RdInt => {
-                match ch {
-                    b'0'..=b'9' => end_pointer += 1,
-                    b'e' => {
-                        end_pointer += 1;
-                        break;
-                    },
-                    _ => { unexpected = true; }
+            State::RdInt => match ch {
+                b'0'..=b'9' => end_pointer += 1,
+                b'e' => {
+                    end_pointer += 1;
+                    break;
+                }
+                _ => {
+                    unexpected = true;
                 }
             },
 
-            State::RdStrLen=> {
+            State::RdStrLen => {
                 match ch {
                     b':' => {
                         state = State::RdStrCon;
-                        let num_str = String::from_utf8(encoded[start_pointer..end_pointer].to_vec()).unwrap();
+                        let num_str =
+                            String::from_utf8(encoded[start_pointer..end_pointer].to_vec())
+                                .unwrap();
 
                         str_len = match num_str.parse::<usize>() {
                             Ok(num) => num,
-                            Err(_) => { unexpected = true; 0 },
+                            Err(_) => {
+                                unexpected = true;
+                                0
+                            }
                         };
                         end_pointer += 1; // now both pointing to first rune
                         start_pointer = end_pointer;
-                    },
-                    b'0'..=b'9' => { end_pointer += 1; },
-                    _ => { unexpected = true; }
+                    }
+                    b'0'..=b'9' => {
+                        end_pointer += 1;
+                    }
+                    _ => {
+                        unexpected = true;
+                    }
                 }
-            },
-            State::RdStrCon=> {
+            }
+            State::RdStrCon => {
                 if str_len > 0 {
                     end_pointer += 1;
                     str_len -= 1;
                 } else {
                     break;
                 }
-            },
+            }
 
-            State::RdLst=> {
-            },
+            State::RdLst => {}
 
-            State::RdDic=> {},
+            State::RdDic => {}
         }
-    };
+    }
 
-    if unexpected { 
+    if unexpected {
         return (Err(()), &encoded[start_pointer..]);
     }
 
     match state {
         State::Ready => {
             return (Err(()), &encoded[start_pointer..]);
-        },
+        }
         State::RdInt => {
             let num_str = String::from_utf8(encoded[start_pointer..end_pointer].to_vec()).unwrap();
 
             return (
                 match num_str.parse::<isize>() {
                     Ok(num) => Ok(BencodeObj::Num(num)),
-                    Err(_) => Err(())
+                    Err(_) => Err(()),
                 },
-                &encoded[end_pointer+1..]
+                &encoded[end_pointer + 1..],
             );
-        },
-        State::RdStrLen => { return (Err(()), &encoded[start_pointer..]); },
+        }
+        State::RdStrLen => {
+            return (Err(()), &encoded[start_pointer..]);
+        }
         State::RdStrCon => {
             return (
-                Ok(BencodeObj::Str(encoded[start_pointer..end_pointer].to_vec())),
-                &encoded[end_pointer..]
+                Ok(BencodeObj::Str(
+                    encoded[start_pointer..end_pointer].to_vec(),
+                )),
+                &encoded[end_pointer..],
             );
-        },
+        }
         State::RdLst => {
             let mut list: Vec<BencodeObj> = vec![];
             let mut remaining = &encoded[start_pointer..];
@@ -124,12 +148,14 @@ pub fn decode(encoded: &[u8]) -> (Result<BencodeObj, ()>, &[u8]/* remaining */) 
                 match val {
                     Ok(bencode_obj) => {
                         list.push(bencode_obj);
-                    },
-                    Err(_) => { return (Err(()), remaining); },
+                    }
+                    Err(_) => {
+                        return (Err(()), remaining);
+                    }
                 }
                 remaining = rem;
             }
-        },
+        }
         State::RdDic => {
             let mut dict: HashMap<Vec<u8>, Box<BencodeObj>> = Default::default();
 
@@ -147,15 +173,15 @@ pub fn decode(encoded: &[u8]) -> (Result<BencodeObj, ()>, &[u8]/* remaining */) 
                 match (key, val) {
                     (Ok(BencodeObj::Str(k)), Ok(v)) => {
                         dict.insert(k, Box::new(v));
-                    },
-                    _ => { return (Err(()), remaining); },
+                    }
+                    _ => {
+                        return (Err(()), remaining);
+                    }
                 }
                 remaining = rem;
             }
-        },
+        }
     }
-
-    (Err(()), &encoded[start_pointer..])
 }
 
 pub fn print_bencode_obj(obj: BencodeObj, indent: Option<usize>) {
@@ -164,19 +190,19 @@ pub fn print_bencode_obj(obj: BencodeObj, indent: Option<usize>) {
     match obj {
         BencodeObj::Num(n) => {
             println!("{:actual_indent$}{}", "", n);
-        },
+        }
         BencodeObj::Str(s) => {
             let maybe_utf8 = String::from_utf8(s.clone());
 
             match maybe_utf8 {
                 Ok(utf_str) => {
                     println!("{:actual_indent$}{:?}", "", utf_str);
-                },
+                }
                 Err(_) => {
                     println!("{:actual_indent$}{:?}", "", "...bytes...");
-                },
+                }
             };
-        },
+        }
         BencodeObj::List(l) => {
             println!("{:actual_indent$}[", "");
 
@@ -185,16 +211,16 @@ pub fn print_bencode_obj(obj: BencodeObj, indent: Option<usize>) {
             }
 
             println!("{:actual_indent$}]", "");
-        },
+        }
         BencodeObj::Dict(d) => {
             println!("{:actual_indent$}{{", "");
-            for (k,v) in d {
+            for (k, v) in d {
                 let i = actual_indent + 2;
                 let k_str = String::from_utf8(k).unwrap();
                 println!("{:i$}{:?}", "", k_str);
                 print_bencode_obj(*v, Some(i));
-            };
+            }
             println!("{:actual_indent$}}}", "");
-        },
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
-#![allow(unused)]
-
 use std::collections::HashMap;
-use std::fs;
 use std::error::Error;
+use std::fs;
 
 mod bencode;
-use bencode::{BencodeObj,print_bencode_obj};
+use bencode::{print_bencode_obj, BencodeObj};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    //let mut encoded = b"li32ei64e5:helloe";
-    //let mut encoded = b"d5:hello3:keye";
-    //let mut encoded = b"d5:item1l12:hellohellobbi32eli32ei32ei32e5:helloi32eee3:key5:valuee";
     let encoded: Vec<u8> = fs::read("sample.torrent")?;
-
 
     let (v, e) = bencode::decode(&encoded);
     print_bencode_obj(v.unwrap(), None);
 
     Ok(())
 }
-
